eda1dc50dfef4f2a273d45700c225ef0cc85cb9e
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2021-08-18 16:01:09 +0100

    [CS] WIP | attempt 1 for PreviousWeightControllerSpec

--- test/controllers/PreviousWeightControllerSpec.scala
+++ test/controllers/PreviousWeightControllerSpec.scala
@@ -1,11 +1,47 @@
 package controllers
 
-import utils.BaseSpec
+import org.mockito.Mockito.{times, verify}
+import play.api.http.Status.OK
+import play.api.libs.json.Json
+import play.api.mvc.ControllerComponents
+import play.api.test.FakeRequest
+import play.api.test.Helpers.{CONTENT_TYPE, contentAsJson, defaultAwaitTimeout, status}
+import services.PreviousWeightService
+import utils.UserDetails._
+import utils._
 
 class PreviousWeightControllerSpec extends BaseSpec {
 
-  "PreviousWeightController" must {
+  val mockPreviousWeightService: PreviousWeightService = mock[PreviousWeightService]
+  val mockControllerComponents: ControllerComponents = mock[ControllerComponents]
+  val controller = new PreviousWeightController(mockPreviousWeightService, mockControllerComponents)
 
+  "PreviousWeightController" must {
+    "return 200 with all weights" in {
+
+      val request = FakeRequest("GET", s"/findAllPreviousWeights/${user.username}")
+        .withHeaders(CONTENT_TYPE -> "application/json")
+
+      val result = controller.findAllPreviousWeights(user.username).apply(request)
+      status(result) mustBe OK
+      (contentAsJson(result) \ "weights").as[String] mustBe Some(previousWeightList)
+
+      verify(mockPreviousWeightService, times(1))
+
+    }
+
+    "return 200 with last weight" in {
+
+      val request = FakeRequest("GET", s"/findAllPreviousWeights/${user.username}")
+        .withHeaders(CONTENT_TYPE -> "application/json")
+
+      val result = controller.findAllPreviousWeights(user.username).apply(request)
+      status(result) mustBe OK
+      (contentAsJson(result) \ "weights").as[String] mustBe Some(previousWeightList)
+
+      verify(mockPreviousWeightService, times(1))
+
+    }
   }
 
 }
