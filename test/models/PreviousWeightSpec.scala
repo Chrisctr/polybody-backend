9d990c0a9ee4f9b4ad1b939e4e80953f92c1c764
Christian-QA <credfernbus@gmail.com>
2021-08-30 14:46:11 +0100

    Fixed model unit tests

--- test/models/PreviousWeightSpec.scala
+++ test/models/PreviousWeightSpec.scala
@@ -1,67 +1,67 @@
-//package models
-//
-//import play.api.libs.json.{JsResultException, Json}
-//import utils.BaseSpec
-//
-//import java.time.LocalDate
-//
-//class PreviousWeightSpec extends BaseSpec {
-//
-//  val previousWeight: PreviousWeight = new PreviousWeight(LocalDate.of(2020, 3, 24), 150.5)
-//
-//  "PreviousWeight" must {
-//
-//    val json = Json.obj(
-//      "dateTime" -> "2020-03-24",
-//      "weight" -> 150.5
-//    )
-//
-//    "deserialise valid values" in {
-//
-//      val result = json.as[PreviousWeight]
-//
-//      result mustBe previousWeight
-//
-//    }
-//
-//    "deserialise invalid values" in {
-//
-//      val invalidJson = Json.obj(
-//        "dateTime" -> "invalidDate",
-//        "weight" -> "150.5"
-//      )
-//
-//      val ex = intercept[JsResultException] {
-//        invalidJson.as[PreviousWeight]
-//      }
-//
-//      ex.getMessage mustBe "JsResultException(errors:List((/dateTime,List(JsonValidationError(List(That's not a date),List()))), (/weight,List(JsonValidationError(List(error.expected.jsnumber),List())))))"
-//    }
-//
-//    "deserialise invalid key" in {
-//
-//      val invalidJson = Json.obj(
-//        "invalidKey" -> "2020-03-24",
-//        "invalidKey" -> "150.5"
-//      )
-//
-//      val ex = intercept[JsResultException] {
-//        invalidJson.as[PreviousWeight]
-//      }
-//
-//      ex.getMessage mustBe "JsResultException(errors:List((/dateTime,List(JsonValidationError(List(error.path.missing),List()))), (/weight,List(JsonValidationError(List(error.path.missing),List())))))"
-//    }
-//
-//    "serialise to json" in {
-//
-//      Json.toJson(previousWeight) mustBe json
-//    }
-//
-//    "serialise/deserialise to the same value" in {
-//
-//      val result = Json.toJson(previousWeight).as[PreviousWeight]
-//
-//      result mustBe previousWeight
-//    }
-//  }
-//}
+package models
+
+import play.api.libs.json.{JsResultException, Json}
+import utils.BaseSpec
+
+import java.time.LocalDate
+
+class PreviousWeightSpec extends BaseSpec {
+
+  val previousWeight: PreviousWeight = new PreviousWeight(LocalDate.of(2020, 3, 24), 150.5)
+
+  "PreviousWeight" must {
+
+    val json = Json.obj(
+      "dateTime" -> "2020-03-24",
+      "weight" -> 150.5
+    )
+
+    "deserialise valid values" in {
+
+      val result = json.as[PreviousWeight]
+
+      result mustBe previousWeight
+
+    }
+
+    "deserialise invalid values" in {
+
+      val invalidJson = Json.obj(
+        "dateTime" -> "invalidDate",
+        "weight" -> "150.5"
+      )
+
+      val ex = intercept[JsResultException] {
+        invalidJson.as[PreviousWeight]
+      }
+
+      ex.getMessage mustBe "JsResultException(errors:List((/dateTime,List(JsonValidationError(List(That's not a date),List()))), (/weight,List(JsonValidationError(List(error.expected.jsnumber),List())))))"
+    }
+
+    "deserialise invalid key" in {
+
+      val invalidJson = Json.obj(
+        "invalidKey" -> "2020-03-24",
+        "invalidKey" -> "150.5"
+      )
+
+      val ex = intercept[JsResultException] {
+        invalidJson.as[PreviousWeight]
+      }
+
+      ex.getMessage mustBe "JsResultException(errors:List((/dateTime,List(JsonValidationError(List(error.path.missing),List()))), (/weight,List(JsonValidationError(List(error.path.missing),List())))))"
+    }
+
+    "serialise to json" in {
+
+      Json.toJson(previousWeight) mustBe json
+    }
+
+    "serialise/deserialise to the same value" in {
+
+      val result = Json.toJson(previousWeight).as[PreviousWeight]
+
+      result mustBe previousWeight
+    }
+  }
+}
